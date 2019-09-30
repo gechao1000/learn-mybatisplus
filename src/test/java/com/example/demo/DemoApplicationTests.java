1f0c11c9ee0926e341a86db5691c8a90c282147c
gexc <gexc@budata.com>
2019-09-30 10:43:57 +0800

    init

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Resource
+    UserMapper userMapper;
+
+    @Test
+    public void contextLoads() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
+
+}
