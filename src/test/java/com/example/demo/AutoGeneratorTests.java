75fce3c4c66f8fce0665168da6e167944533d0e3
gexc <gexc@budata.com>
2019-09-30 11:46:54 +0800

    AutoGenerator Tests

---
+++ src/test/java/com/example/demo/AutoGeneratorTests.java
@@ -0,0 +1,122 @@
+package com.example.demo;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class AutoGeneratorTests {
+
+    private static final String PROJECT_DIR = System.getProperty("user.dir") + "/learn-mybatisplus/";
+
+    @Test
+    public void test() {
+        generate("user", "mobile");
+    }
+
+    private void generate(String... tables) {
+        // 代码生成器
+        AutoGenerator generator = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig global = new GlobalConfig();
+        global.setOutputDir(PROJECT_DIR + "src/main/java");
+        global.setAuthor("gexc");
+        global.setOpen(false);
+        //默认不覆盖，如果文件存在，将不会再生成，配置true就是覆盖
+        global.setFileOverride(true);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        generator.setGlobalConfig(global);
+
+        // 数据源配置
+        DataSourceConfig ds = new DataSourceConfig();
+        ds.setUrl("jdbc:mysql://192.168.80.128:3306/test");
+        ds.setDriverName("com.mysql.cj.jdbc.Driver");
+        // ds.setSchemaName("public");
+        ds.setUsername("root");
+        ds.setPassword("123456");
+        generator.setDataSource(ds);
+
+        // 包配置
+        PackageConfig pkg = new PackageConfig();
+        pkg.setModuleName("db");
+        pkg.setParent("com.example.demo");
+        generator.setPackageInfo(pkg);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setSuperEntityClass("com.baomidou.mybatisplus.samples.generator.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setSuperControllerClass("com.baomidou.mybatisplus.samples.generator.common.BaseController");
+        strategy.setInclude(tables);
+        strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pkg.getModuleName() + "_");
+        strategy.setEntityTableFieldAnnotationEnable(true);
+        generator.setStrategy(strategy);
+
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        generator.setTemplateEngine(new FreemarkerTemplateEngine());
+
+//        configCustomizedCodeTemplate(generator);
+//        configInjection(generator);
+
+        generator.execute();
+    }
+
+    /**
+     * 配置 自定义模板
+     * @param generator
+     */
+    private void configCustomizedCodeTemplate(AutoGenerator generator){
+        TemplateConfig template = new TemplateConfig();
+        // 指定Entity生成使用自定义模板
+        template.setEntity("templates/MyEntityTemplate.java");
+        // 不生成xml
+        template.setXml(null);
+        generator.setTemplate(template);
+    }
+
+    /**
+     * 配置 自定义参数/属性
+     *
+     * @param generator
+     */
+    private void configInjection(AutoGenerator generator){
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<>();
+                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+                this.setMap(map);
+                /*
+                自定义属性注入: 模板配置：abc=${cfg.abc}
+                 */
+            }
+        };
+//        List<FileOutConfig> focList = new ArrayList<>();
+//        focList.add(new FileOutConfig("/templates/MyEntityTemplate.java.ftl") {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 指定模板生，自定义生成文件到哪个地方
+//                return "D:/abc";
+//            }
+//        });
+//        cfg.setFileOutConfigList(focList);
+        generator.setCfg(cfg);
+    }
+}
